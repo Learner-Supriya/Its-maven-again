aaca99679096311d5fb9902ae099cc07c227663c
unknown <supriyaede16@gmail.com>
2022-07-07 11:50:28 +0530

    hello

--- src/main/java/com/example/project/controller/SampleController.java
+++ src/main/java/com/example/project/controller/SampleController.java
@@ -1,8 +1,8 @@
 package com.example.project.controller;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.project.pojoClassExample.greatLearning;
--- src/main/java/com/example/project/pojoClassExample/greatLearning.java
+++ src/main/java/com/example/project/pojoClassExample/greatLearning.java
@@ -2,9 +2,7 @@ package com.example.project.pojoClassExample;
 
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+
 
 @Data
 public class greatLearning {
